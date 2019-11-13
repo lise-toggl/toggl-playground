3f1ae28ad86da7f547d2e1b8ea7869f620c08295
Glitch (toggl-playground) <none>
2019-11-13 00:13:14 +0000

    🍄🏤 Updated with Glitch

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import ProjectExport from "./ProjectExport";
 import WeeklyReport from "./WeeklyReport";
+import ProjectPubliciser from "./ProjectPubliciser";
+import ProjectMakePublic from "./ProjectMakePublic";
 import "../styles/normalize.css";
 import "../styles/barebones.css";
 import "../styles/styles.css";
@@ -11,6 +13,8 @@ const App = () => (
   <Router>
     <Route path="/projectexport" component={ProjectExport} />
     <Route path="/weeklyreport" component={WeeklyReport} />
+    <Route path="/allprojectspublic" component={ProjectMakePublic} />
+    <Route path="/projectpublic" component={ProjectPubliciser} />
     <Route exact path="/" component={Home} />
   </Router>
 );
---
+++ src/components/ProjectPubliciser.js
@@ -0,0 +1,218 @@
+import React, { Component } from "react";
+import Bottleneck from "bottleneck";
+import Api from "../common/api";
+
+const limiter = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 1000
+});
+
+let TOGGL_API;
+
+export default class ProjectPubliciser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ready: false,
+      working: false,
+      apiKey: "",
+      workspaces: [],
+      ws: 0,
+      projects: [],
+      progress: 0
+    };
+  }
+  saveApiKey = e => {
+    const apiKey = e.target ? e.target.value : e;
+
+    this.setState({ apiKey, ready: false });
+    if (apiKey) {
+      localStorage.setItem("apiKey", apiKey);
+      TOGGL_API = new Api(apiKey);
+      limiter.schedule(() => TOGGL_API.get("v8/workspaces")).then(res => {
+        const workspaces = res.filter(ws => ws.admin);
+        this.setState({ workspaces });
+      });
+    } else {
+      localStorage.removeItem("apiKey");
+      this.setState({ workspaces: [] });
+    }
+  };
+  getProjects = async ws => {
+    this.setState({ ws, projects: [], ready: false, working: false });
+
+    const project_data = await limiter.schedule(() =>
+      TOGGL_API.get(`v8/workspaces/${ws}/projects`)
+    );
+    const client_data = await limiter.schedule(() =>
+      TOGGL_API.get(`v8/workspaces/${ws}/clients`)
+    );
+
+    let projects = [];
+    if (project_data != null) {
+      project_data.forEach(i => {
+        if (i.is_private) {
+          let project = {
+            id: i.id,
+            name: i.name
+          };
+          if ("cid" in i) {
+            project.client = client_data.find(x => x.id === i.cid).name;
+          } else {
+            project.client = "(No client)";
+          }
+          projects.push(project);
+        }
+      });
+    }
+    this.setState({ projects, progress: 0, ready: true });
+  };
+  makePublic = () => {
+    this.setState({ working: true });
+    let project_batches = [];
+    for (let i = 0; i < this.state.projects.length; i += 20) {
+      project_batches.push(this.state.projects.slice(i, i + 20));
+    }
+    console.log(project_batches);
+    const baseUrl = `/v9/workspaces/${this.state.ws}/projects/`;
+    const data = [{ op: "replace", path: "/is_private", value: false }];
+    project_batches.forEach(batch => {
+      let url = baseUrl;
+      batch.forEach(project => {
+        url += project.id + ",";
+      });
+      url = url.slice(0, -1);
+      console.log(url);
+      limiter
+        .schedule(() => TOGGL_API.patch(url, data))
+        .then(res => console.log(res));
+    });
+  };
+  // v8 - slow but works
+  // makePublic = () => {
+  //   this.setState({ working: true });
+  //   this.state.projects.forEach(project => {
+  //     limiter
+  //       .schedule(() => TOGGL_API.put(`v8/projects/${project.id}`, {"project":{"is_private":false}}))
+  //       .then(res => {
+  //         console.log(res);
+  //         this.setState(
+  //           prevState => ({
+  //             progress: prevState.progress + 1
+  //           }),
+  //           () => {
+  //             if (this.state.progress == this.state.projects.length) {
+  //               setTimeout(() => this.getProjects(this.state.ws), 3000);
+  //             }
+  //           }
+  //         );
+  //       });
+  //   });
+  // };
+  componentDidMount() {
+    if (localStorage.getItem("apiKey")) {
+      this.saveApiKey(localStorage.getItem("apiKey"));
+    }
+  }
+  renderProgress() {
+    return (
+      <>
+        {this.state.working && "Working... "}
+        {this.state.progress + " / " + this.state.projects.length}
+        {!this.state.working && " DONE!"}
+      </>
+    );
+  }
+  renderAction() {
+    return (
+      <div className="action">
+        {this.state.projects.length > 0 ? (
+          <button
+            className={this.state.working ? "button-primary" : ""}
+            onClick={this.makePublic}
+          >
+            {this.state.working
+              ? this.renderProgress()
+              : "I'm really, really sure about this"}
+          </button>
+        ) : (
+          "There aren't any private projects in here!"
+        )}
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Make all Toggl projects public</h1>
+        </header>
+        <div className="wrapper">
+          <h3>Important notes</h3>
+          <ul>
+            <li>Everyone can see everyone else's time in a public project.</li>
+            <li>
+              This will make <em>all</em> your projects in your selected Toggl
+              workspace public.
+            </li>
+            <li>
+              <strong>You cannot stop the process after it starts!</strong>
+            </li>
+            <li>
+              <strong>There is no way to undo this!</strong>
+            </li>
+            <li>
+              You must be an <strong>admin</strong> user for this to work.
+            </li>
+          </ul>
+          <section>
+            <h4>1. Enter your API key</h4>
+            <p>
+              You can get your API key from the bottom of your Profile page.
+            </p>
+            <div>
+              <input
+                id="apiKey"
+                className="u-full-width"
+                type="text"
+                placeholder="Paste your API key in here"
+                value={this.state.apiKey}
+                onChange={this.saveApiKey}
+              />
+            </div>
+          </section>
+          <section>
+            <h4>2. Choose a workspace</h4>
+            {!this.state.workspaces.length && "Please put in a valid API key!"}
+            {this.state.workspaces.length > 0 && (
+              <div className="buttons">
+                {this.state.workspaces.map((ws, id) => (
+                  <button
+                    key={id}
+                    className={ws.id == this.state.ws ? "button-primary" : ""}
+                    onClick={() => this.getProjects(ws.id)}
+                  >
+                    {ws.name}
+                  </button>
+                ))}
+              </div>
+            )}
+          </section>
+          <section>
+            <h4>3. Make your private projects public</h4>
+            {this.state.ready
+              ? this.renderAction()
+              : "Pick your workspace and wait for your private projects to load."}
+            {this.state.projects.length > 0 && (
+              <ul>
+                {this.state.projects.map((project, id) => (
+                  <li key={id}>{project.name + " - " + project.client}</li>
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
